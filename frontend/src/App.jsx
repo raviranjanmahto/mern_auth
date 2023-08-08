d44f6e8e69406ebcf8492830010bd08701a986aa
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-08-08 17:23:45 +0530

    initial frontend setup and some pakages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Landing from "./screens/Landing";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Landing />
+    </>
+  );
+}
+
+export default App;
